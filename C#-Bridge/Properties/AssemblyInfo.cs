f73f5d07ede97cd866543c8487b7607d8ddff255
voxon-ben <ben@voxon.co>
2019-05-31 13:34:12 +0930

    Removed unused IRuntime, added current interface DLL (for quick building), renamed project to reflect role as C# bridge rather than Unity based

--- C#-Bridge/Properties/AssemblyInfo.cs
+++ C#-Bridge/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BaseVX")]
+[assembly: AssemblyTitle("C#-Runtime")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BaseVX")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("Voxon C#-Runtime Bridge")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
